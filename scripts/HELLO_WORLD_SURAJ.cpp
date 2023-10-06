dd90c83d9297e50b7c0c250dd2fe0ca49ea60a69
Diti2009 <146150347+Diti2009@users.noreply.github.com>
2023-10-06 16:29:38 -0700

    Update HELLO_WORLD_SURAJ.cpp

--- scripts/HELLO_WORLD_SURAJ.cpp
+++ scripts/HELLO_WORLD_SURAJ.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-   cout << "Hello, World!" << endl; // This prints Hello, World!
+   cout << "Hello, World!"
+            "GOOD MORNING!<< endl; // This prints Hello, World!
    return 0;
 }
